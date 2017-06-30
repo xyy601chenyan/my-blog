f1d0b913c18f60354843e6e6f1fe758030c2ddc8
xyy601chenyan <xyy545688506@gmail.com>
2017-06-30 22:02:21 +0800

    Add pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments
   end
-  
+
   root "posts#index"
+
+  #get '/about', to: 'pages#about'
+
+  get '/about' => 'pages#about'
 end
